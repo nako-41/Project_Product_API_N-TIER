6dfa5a5f830cae65710ceb828a7eaa7253889c8d
okan kaipoglu <kaipoglu267@gmail.com>
2022-09-06 14:51:37 +0300

    API kismi kismen bitti,UI katmanı olusturup devam ediliyor

---
+++ ProjectAPI/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using BusinessLayer.Abstract;
+using EntitiesLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductssController : ControllerBase
+    {
+        private readonly IProductService _productManager;
+
+        public ProductssController(IProductService productManager)
+        {
+            _productManager = productManager;
+        }
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _productManager.GetList().ToList();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            if (_productManager.GetList().Where(a => a.Id == id).Count() == 0)
+                return NotFound();
+            else
+
+                try
+                {
+                    return _productManager.GetByID(id);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public ActionResult<bool> Post([FromBody] Product product)
+        {
+            return _productManager.Add(product);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public ActionResult<bool> Put(int id, Product product)
+        {
+            return _productManager.Update(product);
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<bool> Delete(int id)
+        {
+            return _productManager.Delete(id);
+        }
+
+
+    }
+}
--- ProjectAPI/Program.cs
+++ ProjectAPI/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddDbContext<ProjeContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//IoC Container
+
+///////IoC Container///////
 
 // ---------------Service-------------------- /
 
@@ -41,29 +42,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.UseStaticFiles();
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateTime.Now.AddDays(index),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
+//app.Run();
+
+//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
