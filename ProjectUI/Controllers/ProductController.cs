684c69f490bfa66240384854c0beec967661a2f2
okan kaipoglu <kaipoglu267@gmail.com>
2022-09-06 16:07:44 +0300

    grud islemler tamamlandı

--- ProjectUI/Controllers/ProductController.cs
+++ ProjectUI/Controllers/ProductController.cs
@@ -93,5 +93,73 @@ namespace ProjectUI.Controllers
 
             }
         }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5006/");
+                var putTask = client.PutAsJsonAsync($"api/products/{id}", product);
+                putTask.Wait();
+                var result = putTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    ViewBag.ErrorMessage = "Hata Oluştu";
+                    return View(product);
+                }
+            }
+        }
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5006/");
+                var getTask = client.GetAsync($"api/products/{id}");
+                getTask.Wait();
+
+                var result = getTask.Result;
+                var readJsonTask = result.Content.ReadAsAsync<Product>();
+
+                readJsonTask.Wait();
+                Product _product = readJsonTask.Result;
+                return View(_product);
+
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Product product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5006/");
+                var putTask = client.DeleteAsync($"api/products/{id}");
+                putTask.Wait();
+                var result = putTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    ViewBag.ErrorMessage = "Hata Oluştu";
+                    return View();
+                }
+            }
+        }
+
+
     }
 }
